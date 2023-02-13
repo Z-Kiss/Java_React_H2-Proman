91790ad9d6b8ad15a0cc723c2b2e52f76f82c66a
zoty <zoltankiss9210@gmail.com>
2023-02-13 11:05:10 +0100

    add Board modal

--- proman/backend/src/main/java/modal/Board.java
+++ proman/backend/src/main/java/modal/Board.java
@@ -1,2 +1,19 @@
-package modal;public class Board {
+package modal;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "BOARD_ID")
+    private Long id;
+
+    private String boardName;
 }
