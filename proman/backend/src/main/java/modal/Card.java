f0ef9eb3589e017e88ff9fe75a13082cb744d5e6
zoty <zoltankiss9210@gmail.com>
2023-02-13 11:05:51 +0100

    Add Card modal

--- proman/backend/src/main/java/modal/Card.java
+++ proman/backend/src/main/java/modal/Card.java
@@ -1,2 +1,24 @@
-package modal;public class Card {
+package modal;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "CARD_ID")
+    private Long id;
+
+    private String cardTitle;
+
+    private String cardDescription;
+
+    private int order;
+
 }
