5cec26d23b555468f02cfc2b38f687ebc607cbf2
zoty <zoltankiss9210@gmail.com>
2023-02-13 11:05:34 +0100

    add BoardColumn modal

--- proman/backend/src/main/java/modal/BoardColumn.java
+++ proman/backend/src/main/java/modal/BoardColumn.java
@@ -1,4 +1,22 @@
 package modal;
 
-public class Collumn {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class BoardColumn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "COLUMN_ID", nullable = false)
+    private Long id;
+
+    private String columnName;
+
+    private int columnOrder;
+
 }
