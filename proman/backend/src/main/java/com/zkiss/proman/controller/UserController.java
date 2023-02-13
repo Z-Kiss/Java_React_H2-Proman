eca6b3d0580ac46013a562663d90a8016d07bbcf
zoty <zoltankiss9210@gmail.com>
2023-02-13 16:11:46 +0100

    add logout method

--- proman/backend/src/main/java/com/zkiss/proman/controller/UserController.java
+++ proman/backend/src/main/java/com/zkiss/proman/controller/UserController.java
@@ -8,10 +8,7 @@ import com.zkiss.proman.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -62,4 +59,8 @@ public class UserController {
         sessionService.put("userId", userId);
     }
 
+    @GetMapping("/logout")
+    public void userLogout(){
+        sessionService.clear();
+    }
 }
