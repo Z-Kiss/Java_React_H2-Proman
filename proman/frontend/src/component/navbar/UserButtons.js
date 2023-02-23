dde8188ee82629bb201e4f41c36eef47d0313beb
zoty <zoltankiss9210@gmail.com>
2023-02-23 10:11:22 +0100

    add refactored UserButton.js

--- proman/frontend/src/component/navbar/UserButtons.js
+++ proman/frontend/src/component/navbar/UserButtons.js
@@ -6,7 +6,7 @@ import React from "react";
 
 export default function UserButtons({props}) {
 
-    const {setLoggedInUser, setModalContent,loggedInUser,modalContent} = props
+    const {setLoggedInUser, setModalContent,loggedInUser} = props
 
 
     const handleShow = () =>{
@@ -20,35 +20,31 @@ export default function UserButtons({props}) {
     function openLoginModal(){
         setModalContent("login");
         handleShow();
-        console.log(modalContent)
     }
     function openRegisterModal(){
         setModalContent("register");
         handleShow();
-        console.log(modalContent)
-
     }
 
 
+    if(loggedInUser !== null){
+        return (<>
+            <NavDropdown title="Show Boards" id="navbarScrollingDropdown">
+                <NavDropdown.Item>Guest</NavDropdown.Item>
+                <NavDropdown.Divider/>
+                <NavDropdown.Item>Personal</NavDropdown.Item>
+                <NavDropdown.Divider/>
+                <NavDropdown.Item>All</NavDropdown.Item>
+            </NavDropdown>
+            <Button className={"mx-1"} onClick={logout}>Logout</Button>
+            <Button className={"mx-1"}>{loggedInUser}</Button>
+        </>);
+    }else {
+        return(
+        <>
+            <Button onClick={openLoginModal}  className={"mx-1"}>Login</Button>
+            <Button onClick={openRegisterModal} className={"mx-1"}>Register</Button>
+        </>);
+    }
 
-    return (loggedInUser !== null
-            ?
-
-            <>
-                <NavDropdown title="Show Boards" id="navbarScrollingDropdown">
-                    <NavDropdown.Item>Guest</NavDropdown.Item>
-                    <NavDropdown.Divider/>
-                    <NavDropdown.Item>Personal</NavDropdown.Item>
-                    <NavDropdown.Divider/>
-                    <NavDropdown.Item>All</NavDropdown.Item>
-                </NavDropdown>
-                <Button className={"mx-1"} onClick={logout}>Logout</Button>
-                <Button className={"mx-1"}>{loggedInUser}</Button>
-            </>
-            :
-            <>
-                <Button onClick={openLoginModal}  className={"mx-1"}>Login</Button>
-                <Button onClick={openRegisterModal} className={"mx-1"}>Register</Button>
-            </>
-    )
 }
