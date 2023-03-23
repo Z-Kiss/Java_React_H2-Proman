1ebda96bf8371863f81b9868d7ced907a6675644
zoty <zoltankiss9210@gmail.com>
2023-03-23 16:39:41 +0100

    remove Print

--- proman/backend/src/main/java/com/zkiss/proman/controller/BoardColumnController.java
+++ proman/backend/src/main/java/com/zkiss/proman/controller/BoardColumnController.java
@@ -31,7 +31,6 @@ public class BoardColumnController {
 
     @PutMapping("/update")
     public void updateBoardColumn(@RequestBody BoardColumn boardColumn){
-        System.out.println(boardColumn);
         boardColumnService.update(boardColumn);
     }
 
