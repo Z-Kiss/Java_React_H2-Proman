2c4016ee29e84d49837d796e267db60f3f8bf0e1
Zoltan Kiss <zoltankiss9210@gmail.com>
2023-04-19 10:02:03 +0200

    add BoardProvider

---
+++ proman/frontend/src/context/BoardProvider.js
@@ -0,0 +1,38 @@
+import {createContext, useContext} from "react";
+
+
+const BoardContext = createContext({})
+
+const BoardProvider = ({children}) =>{
+
+    const getGuestBoards = async () => {
+        const response = await fetch("/board/get-all-guest-boards")
+        if (response.status === 200) {
+            return await response.json();
+        } else {
+            console.log("nope")
+        }
+    }
+    const getUserBoard = async () => {
+        const response = await fetch("/board/get-all-boards-by-user")
+        if (response.status === 200) {
+            return  await response.json();
+        } else {
+            console.log("nope")
+        }
+    }
+
+    const getBoards ={
+        ofUser:getUserBoard,
+        ofGuest:getGuestBoards
+    }
+
+    return(
+        <BoardContext.Provider value={{getBoards}}>
+            {children}
+        </BoardContext.Provider>
+    )
+}
+
+export const useGetBoards = () => useContext(BoardContext).getBoards;
+export default BoardProvider;
