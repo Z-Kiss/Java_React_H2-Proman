d0c6612110542dbe243c36a74238688d96503f7b
zotya <zoltankiss9210@gmail.com>
2023-05-02 04:01:35 +0200

    Refactire PromanApplication.java

--- proman/backend/src/main/java/com/zkiss/proman/PromanApplication.java
+++ proman/backend/src/main/java/com/zkiss/proman/PromanApplication.java
@@ -4,21 +4,17 @@ import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class PromanApplication {
-	// TODO: Not be able to create things without fields
 
-	public static void main(String[] args) {
-		SpringApplication.run(PromanApplication.class, args);
-	}
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
 
-	@Bean(initMethod = "start", destroyMethod = "stop")
-	public Server h2Server() throws SQLException {
-		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PromanApplication.class, args);
+    }
 }
