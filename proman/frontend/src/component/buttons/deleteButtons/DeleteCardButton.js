c4153a29414b4131742ea24ac08ba9a5527ea5bc
zotya <zoltankiss9210@gmail.com>
2023-05-03 11:00:54 +0200

    extract Create and Delete Card logic from provider to component

--- proman/frontend/src/component/buttons/deleteButtons/DeleteCardButton.js
+++ proman/frontend/src/component/buttons/deleteButtons/DeleteCardButton.js
@@ -1,16 +1,52 @@
-import {useDeleter} from "../../../context/DeleteComponentProvider";
+
 import Button from "react-bootstrap/Button";
 import {RiDeleteBin6Fill} from "react-icons/ri";
+import {useBoards, useSetBoards} from "../../../context/BoardProvider";
 
 export default function DeleteBoardButton({componentId, parentComponentId, boardId}){
 
-    const deleter = useDeleter();
+    const stateOfBoards = useBoards();
+    const setStateOfBoards = useSetBoards();
 
-    const deleteBoard = async () =>{
-        await deleter.ofCard(componentId, parentComponentId, boardId)
+    const deleteCard = async () =>{
+        await cardDeleter(componentId, parentComponentId, boardId)
+    }
+
+    const cardDeleter = async (componentId, parentComponentId, boardId) =>{
+        const response = await deleteCardFromDatabase(componentId);
+        if(response.status === 200){
+            const changedState = deleteCardFromState(componentId, parentComponentId, boardId);
+            setStateOfBoards(changedState);
+        }else{
+            alert("Some problem occurred with the Server try again");
+        }
+    }
+
+    const deleteCardFromDatabase = async (componentId) => {
+        return  await fetch("card/" + componentId,{
+            headers: {
+                Authorization: "Bearer " + sessionStorage.getItem("token")
+            },
+            method: "DELETE"});
+    }
+    const deleteCardFromState = (componentId, parentComponentId, boardId) =>{
+        const copyOfState = [...stateOfBoards]
+        return copyOfState.map(board => {
+            if(board.id === boardId){
+                return {...board, boardColumns: board.boardColumns.map(boardColumn =>{
+                        if(boardColumn.id === parentComponentId){
+                            return {...boardColumn, cards: boardColumn.cards.filter(card => {
+                                    return card.id !== componentId;
+                                })};
+                        }
+                        return boardColumn;
+                    })};
+            }
+            return board;
+        });
     }
 
     return(
-        <Button className={"btn-sm"} variant={"outline-dark"} onClick={deleteBoard} ><RiDeleteBin6Fill/></Button>
+        <Button className={"btn-sm"} variant={"outline-dark"} onClick={deleteCard} ><RiDeleteBin6Fill/></Button>
     )
 }
