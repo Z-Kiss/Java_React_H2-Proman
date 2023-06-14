bb6c3f27daf6abbbb9c6b7a2397fcc57460d2294
Zoltan Kiss <zoltankiss9210@gmail.com>
2023-06-14 15:40:51 +0200

    delete unused import

--- proman/backend/src/main/java/com/zkiss/proman/config/SecurityConfiguration.java
+++ proman/backend/src/main/java/com/zkiss/proman/config/SecurityConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 @EnableWebSecurity
