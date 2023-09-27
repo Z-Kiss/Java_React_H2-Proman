7818cb5881812577d2914d79af371afcc00a8887
zotya <zoltankiss9210@gmail.com>
2023-09-27 14:53:44 +0200

    delete unnecessary endpoint

--- proman/backend/src/main/java/com/zkiss/proman/controller/BoardController.java
+++ proman/backend/src/main/java/com/zkiss/proman/controller/BoardController.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 
@@ -25,11 +24,6 @@ public class BoardController {
     private final BoardService boardService;
     private final JwtService jwtService;
     private final UserService userService;
-
-    @GetMapping()
-    public List<Board> getAllBoards() {
-        return boardService.getAllBoards();
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getAllBoardsByUser(@RequestHeader(HttpHeaders.AUTHORIZATION) String header, @PathVariable("id") UUID id) {
@@ -56,7 +50,7 @@ public class BoardController {
         }
     }
 
-    private boolean hasAuthorization(String header, UUID id) {
+    protected boolean hasAuthorization(String header, UUID id) {
         String token = jwtService.extractToken(header);
         AppUser currentUser = userService.getAppUserByEmail(jwtService.extractEmail(token));
         return currentUser.getId().equals(id);
