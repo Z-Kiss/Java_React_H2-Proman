94b0078b94e0a3252ac1ac8bad4a04a41e7573a9
zotya <zoltankiss9210@gmail.com>
2023-10-02 12:39:16 +0200

    extended UserServiceTest with more test case

--- proman/backend/src/test/java/com/zkiss/proman/service/UserServiceTest.java
+++ proman/backend/src/test/java/com/zkiss/proman/service/UserServiceTest.java
@@ -7,6 +7,7 @@ import com.zkiss.proman.model.DTO.userDTO.UserLoginRequest;
 import com.zkiss.proman.model.DTO.userDTO.UserRegisterRequest;
 import com.zkiss.proman.repository.UserRepository;
 import com.zkiss.proman.utils.TestObjectSupplier;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +15,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -25,6 +28,8 @@ import static org.mockito.Mockito.*;
 class UserServiceTest {
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
     @Mock
     private AuthenticationManager authenticationManager;
     @Mock
@@ -44,7 +49,7 @@ class UserServiceTest {
 
     @Test
     void test_loginUser_method_working() {
-        AppUser appUserTest = this.testObjectSupplier.getGuestAppUserTest();
+        AppUser appUserTest = this.testObjectSupplier.getAppUserTest();
         UserLoginRequest userLoginRequest = this.testObjectSupplier.getLoginRequest();
         when(userRepository.getAppUserByEmail(any())).thenReturn(Optional.of(appUserTest));
 
@@ -54,6 +59,33 @@ class UserServiceTest {
         verify(authenticationManager,times(1)).authenticate(any());
         verify(jwtService,times(1)).generateToken(any());
         Assertions.assertNotNull(authenticationResponse);
+    }
+
+    @Test
+    void test_loginGuestUser_method_working() {
+        AppUser guestAppUserTest = this.testObjectSupplier.getGuestAppUserTest();
+        UserLoginRequest guestLoginRequest = this.testObjectSupplier.getGuestLoginRequest();
+        when(userRepository.getAppUserByEmail(any())).thenReturn(Optional.of(guestAppUserTest));
+
+        AuthenticationResponse authenticationResponse = this.userService.loginUser(guestLoginRequest);
+
+        verify(userRepository,times(2)).getAppUserByEmail(any());
+        verify(authenticationManager,times(1)).authenticate(any());
+        verify(jwtService,times(1)).generateToken(any());
+        Assertions.assertNotNull(authenticationResponse);
+    }
+
+    @Test
+    void test_loginUser_method_throw_exception_when_username_or_password_not_match() {
+        UserLoginRequest userLoginRequest = this.testObjectSupplier.getLoginRequest();
+        when(userRepository.getAppUserByEmail(any())).thenReturn(Optional.empty());
+
+        Exception e =Assertions.assertThrows(BadCredentialsException.class,()->this.userService.loginUser(userLoginRequest));
+        verify(userRepository,times(1)).getAppUserByEmail(any());
+        verify(authenticationManager,times(0)).authenticate(any());
+        verify(jwtService,times(0)).generateToken(any());
+        Assertions.assertEquals(e.getMessage(),"Username/Password mismatch");
+
     }
 
     @Test
@@ -67,6 +99,15 @@ class UserServiceTest {
         Assertions.assertNotNull(appUserFromService);
         Assertions.assertEquals(appUserFromService.getName(), appUserTest.getName());
     }
+    @Test
+    void test_getAppUserById_method_throw_exception_when_user_not_found_by_id() {
+        when(userRepository.findById(any(UUID.class))).thenReturn(Optional.empty());
+        UUID testUUID = UUID.randomUUID();
+
+        Exception e = Assertions.assertThrows(EntityNotFoundException.class,()->this.userService.getAppUserById(testUUID));
+        verify(userRepository,times(1)).findById(any(UUID.class));
+        Assertions.assertEquals(e.getMessage(),"No user with this Id: " + testUUID);
+    }
 
     @Test
     void test_getAppUserByEmail_method_working() {
@@ -78,5 +119,16 @@ class UserServiceTest {
         verify(userRepository,times(1)).getAppUserByEmail(any());
         Assertions.assertNotNull(appUserFromService);
         Assertions.assertEquals(appUserFromService.getId(), appUserTest.getId());
+    }
+
+    @Test
+    void test_getAppUserByEmail_method_throw_exception_when_user_not_found_by_email() {
+        when(userRepository.getAppUserByEmail(any())).thenReturn(Optional.empty());
+        String testEmail = "test1@test1.com";
+
+        Exception e = Assertions.assertThrows(EntityNotFoundException.class,()->userService.getAppUserByEmail(testEmail));
+
+        verify(userRepository,times(1)).getAppUserByEmail(any());
+        Assertions.assertEquals(e.getMessage(),"No user with this email: " + testEmail);
     }
 }
